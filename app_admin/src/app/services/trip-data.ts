012208f3c8959d1d74c920c2a88c8c06f4660252
Cade-Bray <bray.cade@gmail.com>
2025-10-11 15:24:00 -0600

    Add edit-trip component with template and styles; implement trip retrieval and update methods in trip-data service

--- app_admin/src/app/services/trip-data.ts
+++ app_admin/src/app/services/trip-data.ts
@@ -17,7 +17,30 @@ export class TripData {
     return this.http.get<Trip[]>(this.endpoint);
   }
 
+  /**
+   * This function is used to get a single trip code. I admit that I am unsure why the original developer chose to
+   * implement it with an array instead of as a single trip to receive as a parameter. I guess it so if multiple trips
+   * have the same trip code they'll all be returned but this goes against the business logic we implement on the
+   * mongoose model of the trip code being unique.
+   * @param tripCode This is a string of a trip code.
+   */
+  getTrip(tripCode : String): Observable<Trip[]> {
+    return this.http.get<Trip[]>(`${this.endpoint}/${tripCode}`);
+  }
+
+  /**
+   * This function is used to post a trip to the given endpoint.
+   * @param formData Form data is the models/trip object with the information needing to be posted.
+   */
   addTrip(formData: Trip) {
     return this.http.post<Trip>(this.endpoint, formData);
+  }
+
+  /**
+   * This function is used to update a single trip in the database.
+   * @param formData Form data is the models/trip object with the information needed to be PUT.
+   */
+  updateTrip(formData: Trip) : Observable<Trip> {
+    return this.http.put<Trip>(`${this.endpoint}/${formData.code}`, formData);
   }
 }
