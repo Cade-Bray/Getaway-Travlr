25757cc365e23d609f80c965b2b4a485f5f7c3a4
Cade-Bray <bray.cade@gmail.com>
2025-10-12 13:49:56 -0600

    Add edit-trip component with form handling; implement trip retrieval and update methods in trip-data service

--- app_admin/src/app/edit-trip/edit-trip.ts
+++ app_admin/src/app/edit-trip/edit-trip.ts
@@ -1,11 +1,106 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {formatDate, NgClass, TitleCasePipe} from "@angular/common";
+import {Router} from '@angular/router';
+import {TripData} from '../services/trip-data';
+import {Trip} from '../models/trip';
 
 @Component({
   selector: 'app-edit-trip',
-  imports: [],
+  imports: [
+    FormsModule,
+    TitleCasePipe,
+    ReactiveFormsModule,
+    NgClass
+  ],
   templateUrl: './edit-trip.html',
   styleUrl: './edit-trip.css'
 })
-export class EditTrip {
+export class EditTrip implements OnInit{
+  editForm!: FormGroup;
+  submitted = false;
+  message: string = '';
+  trip!: Trip;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private tripService: TripData
+  ) {}
+
+  ngOnInit() {
+    // Retrieving the stashed trip ID
+    let tripCode = localStorage.getItem('tripCode');
+
+    // Error trapping. No trip id found.
+    if (!tripCode) {
+      alert('Something went wrong, couldn\'t find where I stashed the trip code!');
+      this.router.navigate(['']);
+      return;
+    }
+
+    console.log('EditTripComponent::ngOnInit');
+    console.log(`tripcode: ${tripCode}`);
+
+    this.editForm = this.formBuilder.group({
+      _id: [],
+      code: [tripCode, Validators.required],
+      name: ['', Validators.required],
+      length: ['', Validators.required],
+      start: ['', Validators.required],
+      resort: ['', Validators.required],
+      perPerson: ['', Validators.required],
+      image: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+
+    // populate our form with the rest of the values with the get trip from the trip service.
+    this.tripService.getTrip(tripCode)
+      .subscribe({
+        next: (value: any) => {
+          this.trip = value[0];
+
+          // Format the date
+          const formattedTrip = {
+            ...this.trip,
+            start: this.trip.start ? formatDate(this.trip.start, 'yyyy-MM-dd', 'en') : ''
+          };
+
+          // populate the actual fields now that we've got the data.
+          this.editForm.patchValue(formattedTrip);
+
+          // Error Trapping
+          if (this.trip) {
+            this.message = 'No Trip Retrieved from database!';
+          } else {
+            this.message = `Trip: ${tripCode} retrieved`;
+          }
+          console.log(this.message);
+        },
+        error: (error: any) => {
+          console.log(`Error: ${error}`);
+        }
+      });
+  }
+
+  public onSubmit() {
+    this.submitted = true;
+    if (this.editForm.valid) {
+      this.tripService.updateTrip(this.editForm.value)
+        .subscribe({
+          next: (data: any) => {
+            console.log(data);
+            this.router.navigate(['']);
+          },
+          error: (error: any) => {
+            console.log(`Error: ${error}`);
+          }
+        });
+    }
+  }
+
+  // Get the form short name to access the form fields
+  get f() {return this.editForm.controls;}
+
+  protected readonly Object = Object;
 }
--- app_admin/src/app/trip-card/trip-card.ts
+++ app_admin/src/app/trip-card/trip-card.ts
@@ -1,16 +1,27 @@
 import {Component, Input} from '@angular/core';
-import {CurrencyPipe, NgOptimizedImage} from "@angular/common";
+import {CurrencyPipe, DatePipe, NgOptimizedImage} from "@angular/common";
+import {Trip} from '../models/trip';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-trip-card',
-    imports: [
-        CurrencyPipe,
-        NgOptimizedImage
-    ],
+  imports: [
+    CurrencyPipe,
+    NgOptimizedImage,
+    DatePipe
+  ],
   templateUrl: './trip-card.html',
   styleUrl: './trip-card.css'
 })
 export class TripCard {
 
+  constructor(private router: Router) {}
+
   @Input() trips!: any[];
+
+  updateTrip(trip: Trip) {
+    localStorage.removeItem('tripCode');
+    localStorage.setItem('tripCode', trip.code);
+    this.router.navigate(['edit-trip']);
+  }
 }
